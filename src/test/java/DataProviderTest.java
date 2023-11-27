87452770cd4c1a4be513f4fe03940e25ff9bf59f
Lasha Giorgadze <125196379+LashaGG@users.noreply.github.com>
2023-11-28 00:28:39 +0400

    Update DataProviderTest.java

--- src/test/java/DataProviderTest.java
+++ src/test/java/DataProviderTest.java
@@ -7,7 +7,7 @@ public class DataProviderTest {
 
         @DataProvider
         public Object[][] dpMethod(){
-            return new Object[][] {{2, 3 , 5}, {5, 8, 11}};
+            return new Object[][] {{2, 3 , 5}, {5, 8, 13}};
         }
 
         @Test(dataProvider = "dpMethod")
